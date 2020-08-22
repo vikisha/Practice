f1a7a8123b8b54399eea4a11f05b3354be3cdc4e
CodeJoi <63135104+vikisha@users.noreply.github.com>
2020-08-21 23:29:28 -0500

    JS Data Types

---
+++ JS Mod4_DataTypes/js/script.js
@@ -0,0 +1,6 @@
+let $elem = document.getElementById('output');
+$elem.innerHTML += 'Test'+" ";
+$elem.innerHTML += 4 + 6+" ";
+$elem.innerHTML += 4 + 6 + 'Demo' + " ";
+$elem.innerHTML += 'Demo' + 4 + 6 + " ";
+$elem.innerHTML += 'Demo' + (4 + 6) + " ";
