72c54566bcb987daef5c19d3c5ba9cbb495523cd
CodeJoi <63135104+vikisha@users.noreply.github.com>
2020-08-21 23:14:14 -0500

    JS_You Clicked Me Button

---
+++ JavaScript Exercise Week 3_Mod 4/js/script.js
@@ -0,0 +1,9 @@
+function startLogic() {
+    let newHtml = '<em>New Header</em>';
+    document.getElementById('headerItem').innerHTML = newHtml;
+}
+startLogic();
+
+function clickHandler() {
+    alert('You clicked me!');
+}
